fc6a37fab8c4f2d7e925976ed9fe4f4e90ad53e7
Amber Frantz <amber.k.frantz@mgail.com>
2022-08-21 14:27:38 -0400

    Completed Assignment

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,21 @@
 class Author < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :phone_number, length: {is: 10}
 end
+
+
+# create_table "authors", force: :cascade do |t|
+#     t.string "name"
+#     t.string "phone_number"
+#     t.datetime "created_at", precision: 6, null: false
+#     t.datetime "updated_at", precision: 6, null: false
+#   end
+
+#   create_table "posts", force: :cascade do |t|
+#     t.string "title"
+#     t.text "content"
+#     t.text "summary"
+#     t.string "category"
+#     t.datetime "created_at", precision: 6, null: false
+#     t.datetime "updated_at", precision: 6, null: false
+#   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,38 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :clickbait?
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+      ]
+    
+      def clickbait?
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+          errors.add(:title, "must be clickbait")
+        end
+      end
 end
+
+
+
+# create_table "authors", force: :cascade do |t|
+#     t.string "name"
+#     t.string "phone_number"
+#     t.datetime "created_at", precision: 6, null: false
+#     t.datetime "updated_at", precision: 6, null: false
+#   end
+
+#   create_table "posts", force: :cascade do |t|
+#     t.string "title"
+#     t.text "content"
+#     t.text "summary"
+#     t.string "category"
+#     t.datetime "created_at", precision: 6, null: false
+#     t.datetime "updated_at", precision: 6, null: false
+#   end
